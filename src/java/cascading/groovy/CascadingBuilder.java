1eba3dbce0be28995f6b6d8097a99c025141e518
Chris K Wensel <chris@wensel.net>
2009-02-05 17:55:24 -0800

    updated copyright notice

--- src/java/cascading/groovy/CascadingBuilder.java
+++ src/java/cascading/groovy/CascadingBuilder.java
@@ -1,22 +1,13 @@
 /*
- * Copyright (c) 2007-2008 Concurrent, Inc. All Rights Reserved.
+ * Copyright (c) 2009 Concurrent, Inc.
  *
- * Project and contact information: http://www.cascading.org/
+ * This work has been released into the public domain
+ * by the copyright holder. This applies worldwide.
  *
- * This file is part of the Cascading project.
- *
- * Cascading is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Cascading is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Cascading.  If not, see <http://www.gnu.org/licenses/>.
+ * In case this is not legally possible:
+ * The copyright holder grants any entity the right
+ * to use this work for any purpose, without any
+ * conditions, unless such conditions are required by law.
  */
 
 package cascading.groovy;
